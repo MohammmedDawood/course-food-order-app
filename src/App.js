826f00a473b674c2ef305f4e4662af0a970e8d45
Mohammed Dawood <mahmedtony73@gmail.com>
2022-01-09 00:21:05 +0200

    Adding Items and remove duplicates from cart list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [CartIsShown, setCartIsShown] = useState(false);
@@ -14,13 +15,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {CartIsShown && <Cart onCloseCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
